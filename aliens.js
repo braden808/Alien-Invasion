410c08947b05e478cb8901beb474a240e099c4b2
Braden Amorozo <braden808@gmail.com>
2016-10-24 08:15:10 -1000

    update

---
+++ aliens.js
@@ -0,0 +1,21 @@
+var greenAliens = 9000;
+var blueAliens = 500;
+var redAliens = 500;
+var total = greenAliens + blueAliens + redAliens;
+
+if (total == 10000) {
+  console.log("awesome");
+
+   if (greenAliens > 5000){
+  console.log ("why are they so ugly?");
+  }
+
+  if (greenAliens + redAliens > 8000){
+  console.log("no why?????");
+  }
+
+  if (redAliens + blueAliens <= 3000){
+  console.log("okay this seems somewhat manageable, wait no, we're still dead");
+  }
+  
+}
